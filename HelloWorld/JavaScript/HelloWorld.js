fffe02ae49a517d4892981aab78684c02bb33d37
zsbrown97 <zackbrownbandofficial@gmail.com>
2024-04-01 13:10:29 -0500

    JavaScript HelloWorld

---
+++ HelloWorld/JavaScript/HelloWorld.js
@@ -0,0 +1,8 @@
+// To console
+console.log('Hello, World!');
+
+// Alert (displays alert box over the current window)
+// alert("Hello, World!");
+
+// document.write() (Prints content to HTML document)
+// document.write('Hello, World!');
