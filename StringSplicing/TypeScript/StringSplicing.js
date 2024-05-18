58e0d4d9a60c0473358ba5d5d8bab90f08589fc8
zsbrown97 <zackbrownbandofficial@gmail.com>
2024-05-18 18:10:04 -0500

    made stringsplicing great again

---
+++ StringSplicing/TypeScript/StringSplicing.js
@@ -0,0 +1,11 @@
+function mergeAlternately(word1, word2) {
+    var result = '';
+    var mergedLength = word1.length > word2.length ? word1.length : word2.length;
+    for (var i = 0; i < mergedLength; i++) {
+        word1[i] != undefined ? result += word1[i] : word2[i];
+        word2[i] != undefined ? result += word2[i] : word1[i];
+    }
+    return result;
+}
+;
+console.log(mergeAlternately('basket', 'ball'));
--- StringSplicing/TypeScript/StringSplicing.ts
+++ StringSplicing/TypeScript/StringSplicing.ts
@@ -1,20 +1,13 @@
-const word1: string = "Basket";
-const word2: string = "Ball";
+function mergeAlternately(word1: string, word2: string): string {
+  let result: string = '';
+  let mergedLength: number = word1.length > word2.length ? word1.length : word2.length;
 
-let result: string = '';
-let difference: number = 0;
+  for (let i = 0; i < mergedLength; i++) {
+      word1[i] != undefined ? result += word1[i] : word2[i]
+      word2[i] != undefined ? result += word2[i] : word1[i]
+  }
 
-if (word1.length > word2.length) {
-  difference = word1.length;
-} else if (word2.length > word1.length) {
-  difference = word2.length;
-} else if (word1.length == word1.length) {
-  difference = word1.length;
-}
+  return result;
+};
 
-for (let i = 0; i < difference; i++) {
-    word1[i] != undefined ? result += word1[i] : word2[i]
-    word2[i] != undefined ? result += word2[i] : word1[i]
-}
-
-console.log(result);
+console.log(mergeAlternately('basket', 'ball'));
