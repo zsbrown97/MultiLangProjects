3ea4e2fb146d2fe4f8dcec2dc66b97d1a8387132
zsbrown97 <51924558+zsbrown97@users.noreply.github.com>
2024-06-29 16:32:33 -0500

    fixed indents on umactually.java

--- Java/UmActually/UmActually.java
+++ Java/UmActually/UmActually.java
@@ -6,19 +6,19 @@ public class UmActually {
     String finalPhrase = "";
 
     for (int i = 0; i < loweredPhrase.length(); i++) {
-        if (Character.isLetter(loweredPhrase.charAt(i))) {
-            if (i % 3 == 0) {
-                finalPhrase += Character.toUpperCase(loweredPhrase.charAt(i));
-            } else {
-                finalPhrase += loweredPhrase.charAt(i);
-            }
+      if (Character.isLetter(loweredPhrase.charAt(i))) {
+        if (i % 3 == 0) {
+          finalPhrase += Character.toUpperCase(loweredPhrase.charAt(i));
         } else {
-            finalPhrase += loweredPhrase.charAt(i);
+          finalPhrase += loweredPhrase.charAt(i);
         }
+      } else {
+        finalPhrase += loweredPhrase.charAt(i);
+      }
     }
 
     return finalPhrase;
-}
+  }
   public static void main(String[] args) {
     System.out.println(UmActually("Inertia is a property of matter"));
   }
