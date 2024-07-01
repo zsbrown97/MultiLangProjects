2fa7c3e88ab6e247e89088c8688c308c82936c5f
zsbrown97 <51924558+zsbrown97@users.noreply.github.com>
2024-07-01 13:01:12 -0500

    fixed indent

--- C++/UmActually/UmActually.cpp
+++ C++/UmActually/UmActually.cpp
@@ -2,26 +2,26 @@
 #include <regex>
 
 std::string umActually(std::string phrase) {
-    std::transform(phrase.begin(), phrase.end(), phrase.begin(), ::tolower);
-    std::string finalPhrase;
-    std::regex pattern("[^a-zA-Z]");
+  std::transform(phrase.begin(), phrase.end(), phrase.begin(), ::tolower);
+  std::string finalPhrase;
+  std::regex pattern("[^a-zA-Z]");
 
-    for (int i = 0; i < phrase.length(); i++) {
-        if (std::regex_match(phrase, pattern)) {
-            finalPhrase += phrase[i];
-        } else {
-            if (i % 3 == 0) {
-                finalPhrase += std::toupper(phrase[i]);
-            } else {
-                finalPhrase += phrase[i];
-            }
-        }
+  for (int i = 0; i < phrase.length(); i++) {
+    if (std::regex_match(phrase, pattern)) {
+      finalPhrase += phrase[i];
+    } else {
+      if (i % 3 == 0) {
+        finalPhrase += std::toupper(phrase[i]);
+      } else {
+        finalPhrase += phrase[i];
+      }
     }
+  }
 
-    return finalPhrase;
+  return finalPhrase;
 }
 
 int main() {
-    std::cout << umActually("Inertia is a property of matter") << std::endl;
-    return 0;
+  std::cout << umActually("Inertia is a property of matter") << std::endl;
+  return 0;
 }
