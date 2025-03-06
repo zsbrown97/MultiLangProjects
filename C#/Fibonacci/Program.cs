affa9256838c572f9f1c7f455cbc47fd7f4cb22b
zsbrown97 <51924558+zsbrown97@users.noreply.github.com>
2025-03-05 21:35:38 -0600

    c# fibonacci

---
+++ C#/Fibonacci/Program.cs
@@ -0,0 +1,15 @@
+﻿static List<int> Fibonacci(int n) {
+  List<int> result = [1, 1];
+
+  for (int i = 2; i < n; i++) {
+    result.Add(result[i - 1] + result[i - 2]);
+  }
+
+  foreach (int i in result) {
+    Console.WriteLine(i);
+  }
+
+  return result;
+}
+
+Console.WriteLine(Fibonacci(8));
