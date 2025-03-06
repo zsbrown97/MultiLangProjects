a1bdfe62179323869528face020a1813cdedfd85
zsbrown97 <51924558+zsbrown97@users.noreply.github.com>
2025-03-06 13:10:38 -0600

    stringsplicing in C# with rider

---
+++ C#/StringSplicing/Program.cs
@@ -0,0 +1,15 @@
+﻿static string mergeAlternately(string word1, string word2)
+{
+   string result = "";
+   int mergedLength = Math.Max(word1.Length, word2.Length);
+
+   for (int i = 0; i < mergedLength; i++) {
+     if (i < word1.Length) result += word1[i];
+     if (i < word2.Length) result += word2[i];
+   }
+   
+  return result;
+
+}
+
+Console.WriteLine(mergeAlternately("Basket", "Ball"));
