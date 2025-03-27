01f4c4916b9bc8b21aace0b332ff6f6b473f46a9
zsbrown97 <51924558+zsbrown97@users.noreply.github.com>
2025-03-27 10:11:32 -0500

    new solution for containerArea.rs

--- Rust/ContainerArea/containerArea.rs
+++ Rust/ContainerArea/containerArea.rs
@@ -1,25 +1,47 @@
 fn maxArea(height: Vec<i32>) -> i32 {
-  let mut max: i32 = 0;
+  let mut ans = 0;
+  let mut left = 0;
+  let mut right = height.len() - 1;
 
-  for i in 0..height.len() {
-    let mut width: i32 = 1;
+  while left < right {
+      let x = (right - left) as i32;
+      let mut y = 0;
 
-    for j in (i + 1)..height.len() {
-      let mut maxHeight: i32 = height[j];
-      if height[i] < maxHeight {
-        maxHeight = height[i];
+      if height[left] < height[right] {
+          y = height[left];
+          left += 1;
+      } else {
+          y = height[right];
+          right -= 1;
       }
-
-      let area: i32 = maxHeight * width;
-      if area > max {
-        max = area;
-      }
-
-      width += 1;
-    }
+      ans = ans.max(x*y);
   }
-  return max;
+  ans
 }
+
+// Original solution
+// fn maxArea(height: Vec<i32>) -> i32 {
+//   let mut max: i32 = 0;
+
+//   for i in 0..height.len() {
+//     let mut width: i32 = 1;
+
+//     for j in (i + 1)..height.len() {
+//       let mut maxHeight: i32 = height[j];
+//       if height[i] < maxHeight {
+//         maxHeight = height[i];
+//       }
+
+//       let area: i32 = maxHeight * width;
+//       if area > max {
+//         max = area;
+//       }
+
+//       width += 1;
+//     }
+//   }
+//   return max;
+// }
 
 fn main() {
   let case1: Vec<i32> = vec![1,8,6,2,5,4,8,3,7];
